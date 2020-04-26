41616e9bf78bac62b2153731ae99a75762fd0a49
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2020-04-27 01:49:15 +0530

    medicalofficer dashboard created

--- js/main.js
+++ js/main.js
@@ -1,10 +1,1 @@
-var tabs = document.getElementsByClassName('tabs');
 
-function toggleTab(tab){
-    for (var index = 0; index < tabs.length; index++) {
-        tabs[index].style.visibility = "hidden";
-        
-    }
-
-    document.getElementById(tab).style.visibility = "visible";
-}
