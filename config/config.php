04e60977bfd17d5c555069a74af664e1537ddafb
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2020-05-06 18:26:00 +0530

    mvc model updated

--- config/config.php
+++ config/config.php
@@ -1,3 +1,11 @@
 <?php
 
-define('DEFAULT_CONTROLLER', 'Home');
+define('DEBUG', true);
+
+
+define('DEFAULT_CONTROLLER', 'Login');
+define('DEFAULT_LAYOUT' , 'default');
+
+define('PROOT' , '/MOH/');
+
+define('SITE_TITLE', 'Office of Medical Officer of Health');
