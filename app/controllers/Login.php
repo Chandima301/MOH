ea056d14dd08ce1768b6a29f04073f027d9a555e
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2020-05-11 22:09:19 +0530

    mvc applied to login page

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -4,7 +4,7 @@ class Login extends Controller{
     public function __construct($controller, $action){
         parent::__construct($controller, $action);
         $this->load_model('User');
-        $this->view->setLayout('default');
+        $this->view->setLayout('login_layout');
     }
 
     public function indexAction(){
@@ -16,12 +16,16 @@ class Login extends Controller{
             $validation = true;
             if($validation === true){
                 $user = $this->UserModel->findByName($_POST['idcardnum']);
-                if($user && password_verify(Input::get('password'),$user->password)){
+                if($user && password_verify(Input::get('password'),$user->pwd)){
                     $remember = (isset($_POST['remember_me']) && Input::get('remember_me')) ? true :false;
                     $user->login($remember);
                     Router::redirect('');
                 }
             }
         }
+    }
+
+    public function registerAction(){
+        $this->view->render('login/register');
     }
 }
