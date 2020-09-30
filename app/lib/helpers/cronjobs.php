95fd2fd435d97fea675ba4679d364380291d5e7a
Chandima Amarasena <chandimaamarasena.18@cse.mrt.ac.lk>
2020-09-30 21:41:07 +0530

    updated

--- app/lib/helpers/cronjobs.php
+++ app/lib/helpers/cronjobs.php
@@ -5,11 +5,12 @@ require 'E:\Program Files\xammp\htdocs\MOH\app\models\dailyworkReport.php';
 require 'E:\Program Files\xammp\htdocs\MOH\config\config.php';
 require 'E:\Program Files\xammp\htdocs\MOH\core\DB.php';
 require 'E:\Program Files\xammp\htdocs\MOH\app\models\User.php';
-require ''
+require '';
 
 use PHPMailer\PHPMailer\PHPMailer;
 
 /* Exception class. */
+
 require 'PHPMailer\src\Exception.php';
 
 /* The main PHPMailer class. */
@@ -42,30 +43,27 @@ function sendMail($to, $subject, $msg)
     $mail->MsgHTML($content);
     if (!$mail->Send()) {
         echo "Error while sending Email.";
-
     } else {
         echo "Email sent successfully";
     }
-
-    function noticeArea(){
-        $users =new User();
-        $users =$users->getAllusers("MI");
-        foreach( $users as $user){
-            if($user->id){
-                var_dump($user->name);
-                $newDailyworkReport =new dailyworkReport();
-                $newDailyworkReport=$newDailyworkReport->findFirst(["conditions"=>["id =?", "period=?"],"bind"=>[$user->id,strtotime("first day of last month")]]);
-                $coulmn=date('j');
-                $msg="අද දින සායන පැවැත්වීමට නියමිත ප්‍රදේශය ".$newDailyworkReport->{$coulmn};
-                sendMail($user->email, "Daily Work Report Announcement",$msg);
-
 }
 
-
-
+function noticeArea()
+{
+    $users = new User();
+    $users = $users->getAllusers("MI");
+    foreach ($users as $user) {
+        if ($user->id) {
+            var_dump($user->name);
+            $newDailyworkReport = new dailyworkReport();
+            $newDailyworkReport = $newDailyworkReport->findFirst(["conditions" => ["id =?", "period=?"], "bind" => [$user->id, strtotime("first day of last month")]]);
+            $coulmn = date('j');
+            $msg = "අද දින සායන පැවැත්වීමට නියමිත ප්‍රදේශය " . $newDailyworkReport->{$coulmn};
+            sendMail($user->email, "Daily Work Report Announcement", $msg);
         }
     }
 }
+
 function notifyMother()
 {
     $users = new User();
@@ -73,21 +71,19 @@ function notifyMother()
     foreach ($users as $user) {
         if ($user->id) {
             $ICEreport = new ICEmaterial();
-            $ICEreport = $ICEreport->findFirst(["conditions" => ["id =?"],"bind" =>[$user->id]]);
-            $nextClinic = $ICEreport->{28}
-            $nextDate = explode('/',$nextClinic);
+            $ICEreport = $ICEreport->findFirst(["conditions" => ["id =?"], "bind" => [$user->id]]);
+            $nextClinic = $ICEreport->{28};
+            $nextDate = explode('/', $nextClinic);
             $nextMonth = $nextDate[0];
             $nextDay = (string)(int)$nextDate[1];
             $date = explode('/', date('Y/m/d'));
-            $month = $date[0].'/'.$date[1];
+            $month = $date[0] . '/' . $date[1];
             $day = (string)(int)$date[2];
             if ($day == $nextDay) {
-              Helper::dnd("sandaruwan");
+                Helper::dnd("sandaruwan");
             }
         }
     }
-    
-
 }
 
 noticeArea();
