bc89e51cb6a0e0468e7f3145f6a0b8fbadd69683
Indunil <induniludayangana.18@cse.mrt.ac.lk>
2020-10-04 16:23:04 +0530

    sdswd

--- config/config.php
+++ config/config.php
@@ -2,10 +2,10 @@
 
 define('DEBUG', true);
 
-define('DB_NAME', 'moh2');
+define('DB_NAME', 'moh');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:3307');
 
 define('DEFAULT_CONTROLLER', 'Login');
 define('DEFAULT_LAYOUT', 'default');
